e28a6388b05c5f8aff8c2a84af87cfdd850c9f3d
Abijith Viswanath <abijithvv@gmail.com>
2023-11-24 23:07:46 -0500

    api url modified

--- src/main/java/co/abii/learning/jenkinstesttwo/api/Controller.java
+++ src/main/java/co/abii/learning/jenkinstesttwo/api/Controller.java
@@ -2,9 +2,11 @@ package co.abii.learning.jenkinstesttwo.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("springboottwo")
 public class Controller {
 
     @GetMapping("/welcome/{name}")
